0f4d1c2c53813c2266f4fd28a9e4c1ece3db1b3d
Omar <Omar@LAPTOP-BPDEMAGJ>
2021-12-06 18:43:50 -0600

    Add project files.

---
+++ Bible Roulette/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Bible_Roulette
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spinBtn = new System.Windows.Forms.Button();
+            this.NumberPanel = new System.Windows.Forms.Panel();
+            this.Verse2 = new System.Windows.Forms.RichTextBox();
+            this.Verse3 = new System.Windows.Forms.RichTextBox();
+            this.Chapter3 = new System.Windows.Forms.RichTextBox();
+            this.Verse1 = new System.Windows.Forms.RichTextBox();
+            this.Chapter1 = new System.Windows.Forms.RichTextBox();
+            this.Chapter2 = new System.Windows.Forms.RichTextBox();
+            this.Book2 = new System.Windows.Forms.RichTextBox();
+            this.Book1 = new System.Windows.Forms.RichTextBox();
+            this.NumberPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spinBtn
+            // 
+            this.spinBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.spinBtn.Location = new System.Drawing.Point(143, 151);
+            this.spinBtn.Name = "spinBtn";
+            this.spinBtn.Size = new System.Drawing.Size(112, 34);
+            this.spinBtn.TabIndex = 0;
+            this.spinBtn.Text = "SPIN!";
+            this.spinBtn.UseVisualStyleBackColor = false;
+            this.spinBtn.Click += new System.EventHandler(this.spinBtn_Click);
+            // 
+            // NumberPanel
+            // 
+            this.NumberPanel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.NumberPanel.Controls.Add(this.Verse2);
+            this.NumberPanel.Controls.Add(this.Verse3);
+            this.NumberPanel.Controls.Add(this.Chapter3);
+            this.NumberPanel.Controls.Add(this.Verse1);
+            this.NumberPanel.Controls.Add(this.Chapter1);
+            this.NumberPanel.Controls.Add(this.Chapter2);
+            this.NumberPanel.Controls.Add(this.Book2);
+            this.NumberPanel.Controls.Add(this.Book1);
+            this.NumberPanel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumberPanel.Location = new System.Drawing.Point(12, 12);
+            this.NumberPanel.Name = "NumberPanel";
+            this.NumberPanel.Size = new System.Drawing.Size(374, 117);
+            this.NumberPanel.TabIndex = 1;
+            // 
+            // Verse2
+            // 
+            this.Verse2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Verse2.Location = new System.Drawing.Point(281, 21);
+            this.Verse2.Name = "Verse2";
+            this.Verse2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Verse2.Size = new System.Drawing.Size(40, 65);
+            this.Verse2.TabIndex = 7;
+            this.Verse2.Text = "";
+            // 
+            // Verse3
+            // 
+            this.Verse3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Verse3.Location = new System.Drawing.Point(327, 21);
+            this.Verse3.Name = "Verse3";
+            this.Verse3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Verse3.Size = new System.Drawing.Size(38, 65);
+            this.Verse3.TabIndex = 6;
+            this.Verse3.Text = "";
+            // 
+            // Chapter3
+            // 
+            this.Chapter3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Chapter3.Location = new System.Drawing.Point(191, 21);
+            this.Chapter3.Name = "Chapter3";
+            this.Chapter3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Chapter3.Size = new System.Drawing.Size(40, 65);
+            this.Chapter3.TabIndex = 5;
+            this.Chapter3.Text = "";
+            // 
+            // Verse1
+            // 
+            this.Verse1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Verse1.Location = new System.Drawing.Point(237, 21);
+            this.Verse1.Name = "Verse1";
+            this.Verse1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Verse1.Size = new System.Drawing.Size(38, 65);
+            this.Verse1.TabIndex = 4;
+            this.Verse1.Text = "";
+            // 
+            // Chapter1
+            // 
+            this.Chapter1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Chapter1.Location = new System.Drawing.Point(101, 21);
+            this.Chapter1.Name = "Chapter1";
+            this.Chapter1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Chapter1.Size = new System.Drawing.Size(40, 65);
+            this.Chapter1.TabIndex = 3;
+            this.Chapter1.Text = "";
+            // 
+            // Chapter2
+            // 
+            this.Chapter2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Chapter2.Location = new System.Drawing.Point(147, 21);
+            this.Chapter2.Name = "Chapter2";
+            this.Chapter2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Chapter2.Size = new System.Drawing.Size(38, 65);
+            this.Chapter2.TabIndex = 2;
+            this.Chapter2.Text = "";
+            // 
+            // Book2
+            // 
+            this.Book2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Book2.Location = new System.Drawing.Point(55, 21);
+            this.Book2.Name = "Book2";
+            this.Book2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Book2.Size = new System.Drawing.Size(40, 65);
+            this.Book2.TabIndex = 1;
+            this.Book2.Text = "";
+            // 
+            // Book1
+            // 
+            this.Book1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Book1.Location = new System.Drawing.Point(11, 21);
+            this.Book1.Name = "Book1";
+            this.Book1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Book1.Size = new System.Drawing.Size(38, 65);
+            this.Book1.TabIndex = 0;
+            this.Book1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(398, 197);
+            this.Controls.Add(this.NumberPanel);
+            this.Controls.Add(this.spinBtn);
+            this.Name = "MainForm";
+            this.Text = "Bible Roulette";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.NumberPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button spinBtn;
+        private System.Windows.Forms.Panel NumberPanel;
+        private System.Windows.Forms.RichTextBox Chapter3;
+        private System.Windows.Forms.RichTextBox Verse1;
+        private System.Windows.Forms.RichTextBox Chapter1;
+        private System.Windows.Forms.RichTextBox Chapter2;
+        private System.Windows.Forms.RichTextBox Book2;
+        private System.Windows.Forms.RichTextBox Book1;
+        private System.Windows.Forms.RichTextBox Verse2;
+        private System.Windows.Forms.RichTextBox Verse3;
+    }
+}
+
---
+++ Bible Roulette/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bible_Roulette
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void CeroBoxes()
+        {
+            foreach (RichTextBox Richtxb in NumberPanel.Controls.OfType<RichTextBox>())
+            {
+                Richtxb.Text = ResetTextBoxes;
+            }
+        }
+
+        public int RandomUpdateInterval = 50;
+        public const int BookTimeLength = 5000;
+        public const int ChapterTimeLength = 10000;
+        public const int VerseTimeLength = 15000;
+        public volatile int ElapsedTime = 0;
+        public const int ResetTime = 0;
+        public const string ResetTextBoxes = "0";
+        public const int MaxBook = 7;
+        public const int MinBook = 0;
+        public const int MinChapter = 0;
+        public const int MaxChapterOne = 2;
+        public const int MaxChapterTwo = 10;
+        public const int MaxChapterThree = 10;
+        public const int MinVerse = 0;
+        public const int MaxVerseOne = 2;
+        public const int MaxVerseTwo = 10;
+        public const int MaxVerseThree = 10;
+
+        public void RandomizeBox()
+        {
+
+            spinBtn.BackColor = SystemColors.ActiveCaption;
+            spinBtn.Refresh();
+
+            Random RandomForBookOne;
+            Random RandomForBookTwo ;
+
+            Random RandomForChapterOne;
+            Random RandomForChapterTwo ;
+            Random RandomForChapterThree ;
+
+            Random RandomForVerseOne ;
+            Random RandomForVerseTwo ;
+            Random RandomForVerseThree ;
+
+            ElapsedTime = ResetTime;
+            
+            while (ElapsedTime <= VerseTimeLength)
+            {
+
+                if (ElapsedTime <= BookTimeLength)
+                {
+
+                    RandomForBookOne = new Random();
+                    RandomForBookTwo = new Random();
+
+                    Book1.Text = RandomForBookOne.Next(MinBook, MaxBook).ToString();
+                    Book2.Text = RandomForBookTwo.Next(MinBook, MaxBook).ToString();
+
+
+                    RandomForBookOne = null;
+                    RandomForBookTwo = null;
+            
+                    Book1.Refresh();
+                    Book2.Refresh();
+
+                }
+
+                if(ElapsedTime <= ChapterTimeLength)
+                {
+                    RandomForChapterOne = new Random();
+                    RandomForChapterTwo = new Random();
+                    RandomForChapterThree = new Random();
+
+                    Chapter1.Text = RandomForChapterOne.Next(MinChapter, MaxChapterOne).ToString();
+                    Chapter2.Text = RandomForChapterTwo.Next(MinChapter, MaxChapterTwo ).ToString();
+                    Chapter3.Text = RandomForChapterThree.Next(MinChapter, MaxChapterThree).ToString();
+
+                    RandomForChapterOne = null;
+                    RandomForChapterTwo = null;
+                    RandomForChapterThree = null;
+
+                    Chapter1.Refresh();
+                    Chapter2.Refresh();
+                    Chapter3.Refresh();
+
+                }
+
+                RandomForVerseOne = new Random();
+                RandomForVerseTwo = new Random();
+                RandomForVerseThree = new Random();
+
+                Verse1.Text = RandomForVerseOne.Next(MinVerse , MaxVerseOne).ToString();
+                Verse2.Text = RandomForVerseTwo.Next(MinVerse, MaxVerseTwo).ToString();
+                Verse3.Text = RandomForVerseThree.Next(MinVerse, MaxVerseThree).ToString();
+
+                RandomForVerseOne = null;
+                RandomForVerseTwo = null;
+                RandomForVerseThree = null;
+
+                Verse1.Refresh();
+                Verse2.Refresh();
+                Verse3.Refresh();
+
+                ElapsedTime += RandomUpdateInterval;
+
+            }
+
+            CheckResult();
+          
+        }
+
+        public void CheckResult()
+        {
+
+            int ChapterNumber = int.Parse(Chapter1.Text + Chapter2.Text + Chapter3.Text) ;
+            int BookNumber = int.Parse(Book1.Text + Book2.Text);
+            int VerseNumber = int.Parse(Verse1.Text + Verse2.Text + Verse3.Text) ;
+            bool isReal = false;
+            String BookName = "";
+            int MaxChapters, MaxVerses;
+
+            if (BookList.TryGetValue( BookNumber, out BookName ) )
+            {
+
+                MaxChapters = NumberOfChaptersInBook[BookName];
+
+                if (ChapterNumber <= MaxChapters)
+                {
+                    //TODO check the amount of verses in a given chapter
+                    isReal = true;
+                }
+
+            }
+
+            if ((BookNumber == 0) || (VerseNumber == 0)) { isReal = false; }
+
+            if (isReal)
+            {
+                spinBtn.BackColor = Color.Green;
+                MessageBox.Show(BookName + " " + ChapterNumber.ToString() + ":" + VerseNumber.ToString() + "?");
+            }
+            else
+            {
+                spinBtn.BackColor = Color.Red;
+            }
+
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            CeroBoxes();
+        }
+
+        private void spinBtn_Click(object sender, EventArgs e)
+        {
+            RandomizeBox();
+        }
+
+        public Dictionary<String, int> NumberOfChaptersInBook = new Dictionary<String, int>()
+        {
+            {"Genesis", 50        },
+            {"Exodus", 40         },
+            {"Leviticus", 27      },
+            {"Numbers", 36        },
+            {"Deuteronomy", 34    },
+            {"Joshua", 24         },
+            {"Judges", 21         },
+            {"Ruth",    4         },
+            {"1 Samuel",    31    },
+            {"2 Samuel",  24      },
+            {"1 Kings", 22        },
+            {"2 Kings", 25        },
+            {"1 Chronicles", 29   },
+            {"2 Chronicles",  36  },
+            {"Ezra",    10        },
+            {"Nehemiah",    13    },
+            {"Esther",  10        },
+            {"Job", 42            },
+            {"Psalms",  150       },
+            {"Proverbs",  31      },
+            {"Ecclesiastes",  12  },
+            {"Song of Solomon", 8 },
+            {"Isaiah",  66        },
+            {"Jeremiah",    52    },
+            {"Lamentations",    5 },
+            {"Ezekiel", 48        },
+            {"Daniel",  12        },
+            {"Hosea",   14        },
+            {"Joel",    3         },
+            {"Amos",    9         },
+            {"Obadiah", 1         },
+            {"Jonah",   4         },
+            {"Micah",   7         },
+            {"Nahum",   3         },
+            {"Habakkuk",  3       },
+            {"Zephaniah",   3     },
+            {"Haggai",  2         },
+            {"Zechariah",   14    },
+            {"Malachi", 4         },
+            {"Matthew", 28        },
+            {"Mark",    16        },
+            {"Luke",    24        },
+            {"John",    21        },
+            {"The Acts", 28       },
+            {"Romans",  16        },
+            {"1 Corinthians", 16  },
+            {"2 Corinthians", 13  },
+            {"Galatians",   6     },
+            {"Ephesians",   6     },
+            {"Philippians", 4     },
+            {"Colossians",  4     },
+            {"1 Thessalonians", 5 },
+            {"2 Thessalonians", 3 },
+            {"1 Timothy",   6     },
+            {"2 Timothy",   4     },
+            {"Titus",   3         },
+            {"Philemon",    1     },
+            {"Hebrews", 13        },
+            {"James",   5         },
+            {"1 Peter",  5        },
+            {"2 Peter",  3        },
+            {"1 John",  5         },
+            {"2 John",  1         },
+            {"3 John",  1         },
+            {"Jude",    1         },
+            {"Revelation",  22    }
+        };
+        
+        public Dictionary<int, String> BookList = new Dictionary<int, String>()
+        {
+            {1, "Genesis"          },
+            {2, "Exodus"           },
+            {3, "Leviticus"        },
+            {4, "Numbers"          },
+            {5, "Deuteronomy"      },
+            {6, "Joshua"           },
+            {7, "Judges"           },
+            {8, "Ruth"             },
+            {9, "1 Samuel"         },
+            {10, "2 Samuel"        },
+            {11, "1 Kings"         },
+            {12, "2 Kings"         },
+            {13, "1 Chronicles"    },
+            {14, "2 Chronicles"    },
+            {15, "Ezra"            },
+            {16, "Nehemiah"        },
+            {17, "Esther"          },
+            {18, "Job"             },
+            {19, "Psalms"          },
+            {20, "Proverbs"        },
+            {21, "Ecclesiastes"    },
+            {22, "Song of Solomon" },
+            {23, "Isaiah"          },
+            {24, "Jeremiah"        },
+            {25, "Lamentations"    },
+            {26, "Ezekiel"         },
+            {27, "Daniel"          },
+            {28, "Hosea"           },
+            {29, "Joel"            },
+            {30, "Amos"            },
+            {31, "Obadiah"         },
+            {32, "Jonah"           },
+            {33, "Micah"           },
+            {34, "Nahum"           },
+            {35, "Habakkuk"        },
+            {36, "Zephaniah"       },
+            {37, "Haggai"          },
+            {38, "Zechariah"       },
+            {39, "Malachi"         },
+            {40, "Matthew"         },
+            {41, "Mark"            },
+            {42, "Luke"            },
+            {43, "John"            },
+            {44, "The Acts"        },
+            {45, "Romans"          },
+            {46, "1 Corinthians"   },
+            {47, "2 Corinthians"   },
+            {48, "Galatians"       },
+            {49, "Ephesians"       },
+            {50, "Philippians"     },
+            {51, "Colossians"      },
+            {52, "1 Thessalonians" },
+            {53, "2 Thessalonians" },
+            {54, "1 Timothy"       },
+            {55, "2 Timothy"       },
+            {56, "Titus"           },
+            {57, "Philemon"        },
+            {58, "Hebrews"         },
+            {59, "James"           },
+            {60, "1 Peter"         },
+            {61, "2 Peter"         },
+            {62, "1 John"          },
+            {63, "2 John"          },
+            {64, "3 John"          },
+            {65, "Jude"            },
+            {66, "Revelation"      }
+        };
+
+    }
+}
